bf2f7004993c0bacd77bceb9b4657b1b36154860
Kamil Mrzygłód <kmrzyglod@moneygram.com>
2016-04-05 10:01:16 +0200

    ModuleWeaver structure, added test project

--- Scythe.Fody/ModuleWeaver.cs
+++ Scythe.Fody/ModuleWeaver.cs
@@ -1,6 +1,16 @@
-﻿namespace Scythe.Fody
+﻿using System.Xml.Linq;
+using Mono.Cecil;
+
+namespace Scythe.Fody
 {
     public class ModuleWeaver
     {
+        public ModuleDefinition ModuleDefinition { get; set; }
+
+        public XElement Config { get; set; }
+
+        public void Execute()
+        {
+        }
     }
 }
