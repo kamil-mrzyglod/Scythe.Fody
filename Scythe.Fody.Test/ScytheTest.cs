e9d066426e1fba67eb257310c92c19bc9cca8db9
Kamil Mrzygłód <kmrzyglod@moneygram.com>
2016-04-05 10:56:32 +0200

    Full solution structure ready to develop

---
+++ Scythe.Fody.Test/ScytheTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace Scythe.Fody.Test
+{
+    public class ScytheTest
+    {
+        private const string AssemblyPath = @"../../../../Scythe.TestAssembly/bin/Debug/Scythe.TestAssembly.dll";
+
+        private string _weavedAssemblyName;
+
+        private Assembly _assembly;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _weavedAssemblyName = AssemblyDirectory + $"Scythe.TestAssembly{DateTime.Now.Ticks}.dll";
+
+            var md = ModuleDefinition.ReadModule(Path.GetFullPath(AssemblyDirectory + AssemblyPath));
+            var weaver = new ModuleWeaver { ModuleDefinition = md };
+
+            weaver.Execute();
+            md.Write(_weavedAssemblyName);
+
+            _assembly = Assembly.LoadFile(_weavedAssemblyName);
+        }
+
+        private static string AssemblyDirectory
+        {
+            get
+            {
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new UriBuilder(codeBase);
+                var path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+    }
+}
