dcaf723855415b3e43a98b0af9346eff71387c7b
Kamil Mrzygłód <kamil.mrzyglod@codenova.pl>
2016-04-06 15:06:14 +0200

    cyclomatic-complexity Cyclomatic complexity implementation

---
+++ Scythe.CyclomaticComplexityTest/TestClass.cs
@@ -0,0 +1,63 @@
+﻿namespace Scythe.CyclomaticComplexityTest
+{
+    public class TestClass
+    {
+        public int ValidMethod(int foo)
+        {
+            if (foo == 1) return foo;
+            if (foo == 2) return foo + 2;
+            if (foo == 3) return foo + 3;
+            return foo;
+        }
+
+        public int ValidMethod2(int foo)
+        {
+            if (foo == 1) foo += 1;
+            if (foo == 2) foo += 2;
+            if (foo == 3) foo += 3;
+            return foo;
+        }
+
+        public int ValidMethod3(int foo)
+        {
+            if (foo == 1)
+            {
+                foo += 1;
+            }
+            else
+            {
+                foo += foo;
+            }
+
+            return foo;
+        }
+
+        public int ValidMethod4(int foo)
+        {
+            if (foo == 1)
+            {
+                foo += 1;
+            }
+            else if (foo == 2)
+            {
+                foo *= foo;
+            }
+            else
+            {
+                foo += foo;
+            }
+
+            return foo;
+        }
+
+        public int InvalidMethod(int foo)
+        {
+            if (foo == 1) return foo;
+            if (foo == 2) return foo + 2;
+            if (foo == 3) return foo + 3;
+            if (foo == 4) return foo + 4;
+            if (foo == 5) return foo + 5;
+            return foo;
+        }
+    }
+}
---
+++ Scythe.Fody/Scythes/CyclomaticComplexity.cs
@@ -0,0 +1,55 @@
+﻿namespace Scythe.Fody.Scythes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    using Mono.Cecil;
+    using Mono.Cecil.Cil;
+
+    public class CyclomaticComplexity : IScythe
+    {
+        /// <summary>
+        /// Checks whether cyclomatic complexity(https://en.wikipedia.org/wiki/Cyclomatic_complexity)
+        /// of a method doesn't exceed predefinied threshold. 
+        /// Note that current implementation is a bit different
+        /// that a mathematical equation - it checks
+        /// for a control transfers OpCodes and counts them
+        /// to determine complexity of a method.
+        /// </summary>
+        public IEnumerable<ErrorMessage> Check(MethodDefinition definition, XElement config)
+        {
+            var element = config.Elements("CyclomaticComplexity").FirstOrDefault();
+            if (element == null)
+                yield break;
+
+            if (definition.Body.Instructions.All(DoNotTransferControl()))
+                yield break;
+
+            var cyclomaticComplexity = element.Attribute("CyclomaticComplexity").Value;
+            var severity = element.Attribute("Severity").Value;
+            var transfers =
+                definition.Body.Instructions.Count(
+                    _ =>
+                    _.OpCode == OpCodes.Brtrue || _.OpCode == OpCodes.Brtrue_S || _.OpCode == OpCodes.Brfalse
+                    || _.OpCode == OpCodes.Brfalse_S);
+
+            if (definition.Body.Instructions.Any(_ => _.OpCode == OpCodes.Br || _.OpCode == OpCodes.Br_S)) transfers++;
+
+            if(transfers > int.Parse(cyclomaticComplexity))
+                yield return
+                    new ErrorMessage(
+                        $"Method {definition.FullName} has cyclomatic complexity of {transfers} while max is {cyclomaticComplexity}",
+                        ErrorType.CyclomaticComplexity,
+                        severity);
+        }
+
+        private static Func<Instruction, bool> DoNotTransferControl()
+        {
+            return _ =>
+                   _.OpCode != OpCodes.Brtrue && _.OpCode != OpCodes.Brtrue_S && _.OpCode != OpCodes.Brfalse
+                   && _.OpCode != OpCodes.Brfalse_S;
+        }
+    }
+}
